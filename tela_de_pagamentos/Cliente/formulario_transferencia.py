7d5a172e14becf9d7acc4f977d98eaa553eb5baa
Rodrigo Matos <rodrigo.matos62@aluno.ifce.edu.br>
2024-09-12 14:27:31 -0300

    Atualização

--- tela_de_pagamentos/Cliente/formulario_transferencia.py
+++ tela_de_pagamentos/Cliente/formulario_transferencia.py
@@ -1,6 +1,5 @@
 import sqlite3, os
 from datetime import datetime
-from comprovante_pagamento import emissao_comprovante
 
 #Função para criar o banco de dados das transferências
 def criar_tabela():
@@ -46,6 +45,24 @@ def verificacao(nome, conta):
     resultado = cursor.fetchone()
     conexao.close()
     return resultado is not None
+
+def emissao_comprovante(id, nome, cpf, banco, agencia, conta, valor, data):
+    diretorio_atual = os.path.abspath(os.path.dirname(__file__))
+    caminho = os.path.join(diretorio_atual, f'comprovante_{id}.txt')
+
+    with open(caminho, 'w', encoding='utf-8') as arquivo:
+        arquivo.write("==== Comprovante de Transferência ====\n")
+        arquivo.write(f"==== ID da Transação: {id}\n")
+        arquivo.write(f"==== Nome: {nome}\n")
+        arquivo.write(f"==== CPF: {cpf}\n")
+        arquivo.write(f"==== Banco: {banco}\n")
+        arquivo.write(f"==== Agência: {agencia}\n")
+        arquivo.write(f"==== Conta: {conta}\n")
+        arquivo.write(f"==== Valor: {valor}\n")
+        arquivo.write(f"==== Data: {data}\n")
+        arquivo.write("======================================\n")
+
+    print(f"Comprovante emitido: {caminho}")
 
 #Função para validação do cpf
 def validar_cpf(cpf):
